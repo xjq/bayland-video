6546cb54b95ee013bd5f486394e304e2b2f3c2ca
xjq <xjq314@gmail.com>
2026-02-01 17:26:39 +0800

    fix server port and IP

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    host: '0.0.0.0',
+    port: 80,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
